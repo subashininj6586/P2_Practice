1ed498a4df4dde8190ce0fd2e53b1eb15ffc737a
Subashininj <83811974+subashininj6586@users.noreply.github.com>
2021-05-13 12:04:24 +0530

    Add files via upload
    Created component node,js in component folder and used inside app.js file

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
 import React from 'react'
+import Note from './component/Note'
 
-const App = (props) => {
-  const { notes } = props
 
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-      
-      {notes.map(note => 
-          <li key={note.id}>
-            {note.content}
-          </li>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
         )}
-     
       </ul>
     </div>
   )
 }
-
 export default App
